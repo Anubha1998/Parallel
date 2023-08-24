05d40df7b92e4115fb7181748435915b0691c2c2
lambdatestsupport <134601151+lambdatestsupport@users.noreply.github.com>
2023-08-24 18:34:11 +0530

    Update TestNGTodo1.java

--- src/test/java/com/lambdatest/TestNGTodo1.java
+++ src/test/java/com/lambdatest/TestNGTodo1.java
@@ -26,9 +26,9 @@ public class TestNGTodo1 {
         String hub = "@hub.lambdatest.com/wd/hub";
 
         DesiredCapabilities caps = new DesiredCapabilities();
-        caps.setCapability("platform", "MacOS Catalina");
-        caps.setCapability("browserName", "Safari");
-        caps.setCapability("version", "latest");
+        caps.setCapability("browserName", device);
+        caps.setCapability("browserVersion",version);
+        caps.setCapability("platform", platform);
         caps.setCapability("build", "TestNG With Java");
         caps.setCapability("name", m.getName() + " - " + this.getClass().getName());
         caps.setCapability("plugin", "git-testng");
