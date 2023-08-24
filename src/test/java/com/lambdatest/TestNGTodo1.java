35b43d5d9554e5f29990ddb238b2ba08c5fad8e7
lambdatestsupport <134601151+lambdatestsupport@users.noreply.github.com>
2023-08-24 19:06:52 +0530

    Update TestNGTodo1.java

--- src/test/java/com/lambdatest/TestNGTodo1.java
+++ src/test/java/com/lambdatest/TestNGTodo1.java
@@ -20,7 +20,7 @@ public class TestNGTodo1 {
 
     @BeforeMethod
     @org.testng.annotations.Parameters(value = {"browser", "version", "platform"})
-    public void TestNGTod1(String device, String version, String platform) {
+    public void TestNGTod1(String browser, String version, String platform) {
         String username = System.getenv("LT_USERNAME") == null ? "Your LT Username" : System.getenv("LT_USERNAME");
         String authkey = System.getenv("LT_ACCESS_KEY") == null ? "Your LT AccessKey" : System.getenv("LT_ACCESS_KEY");
         String hub = "@hub.lambdatest.com/wd/hub";
